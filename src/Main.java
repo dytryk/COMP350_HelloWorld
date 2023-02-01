c8d1cea2d3b3d5d076cfb2f00bd719502e5d12d8
JUDGEPM20 <pjudge2002@gmail.com>
2023-02-01 14:37:04 -0500

    new stuff

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main
 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(hello("jon"));
+    }
+
+    private static String hello(String name){
+        return "Hello " + name + "!";
     }
 }
